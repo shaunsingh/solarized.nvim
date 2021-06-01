f967011e6a9dba835f1f5ad1f2e23d385e4d07c9
shaunsingh <71196912+shaunsingh@users.noreply.github.com>
2021-06-01 12:42:00 -0400

    Update util.lua

--- lua/solarized/util.lua
+++ lua/solarized/util.lua
@@ -1,5 +1,5 @@
 local util = {}
-local solarized = require('moonlight.theme')
+local solarized = require('solarized.theme')
 
 -- Go trough the table and highlight the group with the color values
 util.highlight = function (group, color)
